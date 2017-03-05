16d346a0a8ba61e4c326934f25dc6e89eff5ff2a
ryanj89 <ryanj89@gmail.com>
2017-03-04 18:19:55 -0700

    created /comments CRUDL routes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const morgan = require('morgan');
 const express = require('express');
 const bodyParser = require('body-parser');
 const posts = require('./routes/posts');
+const comments = require('./routes/comments');
 
 const app = express();
 const port = process.env.PORT || 8000;
@@ -9,7 +10,9 @@ const port = process.env.PORT || 8000;
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use('/posts', posts);
+app.use('/comments', comments);
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
